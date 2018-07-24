21f97a0fb4f9034f39e45a21b1e1691c14f388f5
Daniel Clarke <u8sand@gmail.com>
2018-07-24 16:58:31 -0400

    Improved serializers
    - Automatically set authors/assessors
    - Readonly fields
    - Exclusions

--- FAIRshakeAPI/serializers.py
+++ FAIRshakeAPI/serializers.py
@@ -2,35 +2,73 @@ from rest_framework import serializers
 from . import models
 
 class IdentifiableModelMixinSerializer(serializers.ModelSerializer):
+  def create(self, validated_data):
+    user = self.context.get('request').user
+    validated_data.update(
+      authors=[
+        user.id,
+      ],
+    )
+    return super(IdentifiableModelMixinSerializer, self).create(validated_data)
+  
+  def update(self, instance, validated_data):
+    user = self.context.get('request').user
+    validated_data.update(
+      authors=list(set(validated_data.get('authors', [])+[user.id])),
+    )
+    return super(IdentifiableModelMixinSerializer, self).update(instance, validated_data)
+
   class Meta:
-    model = models.IdentifiableModelMixin
-    fields = '__all__'
     abstract = True
 
 class DigitalObjectSerializer(IdentifiableModelMixinSerializer):
   class Meta:
     model = models.DigitalObject
     fields = '__all__'
+    read_only_fields = (
+      'authors',
+    )
 
 class ProjectSerializer(IdentifiableModelMixinSerializer):
   class Meta:
     model = models.Project
-    fields = '__all__'
+    exclude = (
+      'digital_objects',
+    )
+    read_only_fields = (
+      'authors',
+    )
 
 class MetricSerializer(IdentifiableModelMixinSerializer):
   class Meta:
     model = models.Metric
     fields = '__all__'
+    read_only_fields = (
+      'authors',
+    )
 
 class RubricSerializer(IdentifiableModelMixinSerializer):
   class Meta:
     model = models.Rubric
     fields = '__all__'
+    read_only_fields = (
+      'authors',
+    )
 
 class AssessmentSerializer(serializers.ModelSerializer):
+  assessor = serializers.PrimaryKeyRelatedField(
+    read_only=True,
+    default=serializers.CurrentUserDefault(),
+  )
+
   class Meta:
     model = models.Assessment
     fields = '__all__'
+    read_only_fields = (
+      'requestor',
+      'assessor',
+      'timestamp',
+    )
 
 class AnswerSerializer(serializers.ModelSerializer):
   class Meta:
@@ -47,4 +85,4 @@ class ScoreSerializer(serializers.BaseSerializer):
 class DigitalObjectsToRubricsSerializer(serializers.ModelSerializer):
   class Meta:
     model = models.DigitalObject
-    fields = ('rubrics',)
+    fields = ('id', 'rubrics',)
