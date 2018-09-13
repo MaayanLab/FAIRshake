cad977fc10e3ef2eab7b509e1333821742270bf8
Daniel J. B. Clarke <u8sand@gmail.com>
2018-09-13 15:44:40 -0400

    CustomUUIDField handle None value

--- extensions/versions_ex/fields.py
+++ extensions/versions_ex/fields.py
@@ -4,7 +4,11 @@ from django.db.models import UUIDField
 
 class CustomUUIDField(UUIDField):
   def to_python(self, value):
+    if value is None:
+      return None
     return uuid.UUID(bytes=base64.urlsafe_b64decode((value + '==').replace('_', '/')))
 
   def from_db_value(self, value, expr, connection):
+    if value is None:
+      return None
     return base64.urlsafe_b64encode(value.bytes).decode("utf-8").rstrip('=\n').replace('/', '_')
