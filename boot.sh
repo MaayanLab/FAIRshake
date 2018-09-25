25dfbb3ae98fe709a6a9f7dddf20c3db96842d42
Daniel J. B. Clarke <u8sand@gmail.com>
2018-09-25 17:00:36 -0400

    Fixed legacy endpoint redirect

--- boot.sh
+++ boot.sh
@@ -96,8 +96,8 @@ http {
         location ~ ^/v2/ {
             rewrite ^/v2/(.*)$ https://\$server_name/\$1 redirect;
         }
-        location ~ ^/assessment/add/(.*)$ {
-            rewrite ^/assessment/perform/(.*)$ https://\$server_name/\$1 redirect;
+        location ~ ^/assessment/add/ {
+            rewrite ^/assessment/add/(.*)$ https://\$server_name/assessment/perform/\$1 redirect;
         }
         location ~ ^/static/v2/ {
             rewrite ^/static/v2/(.*)$ https://\$server_name/v2/static/\$1 redirect;
