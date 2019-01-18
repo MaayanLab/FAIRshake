893ae246f1ca8d37288448423ba1a6a23729220f
Daniel J. B. Clarke <u8sand@gmail.com>
2019-01-18 17:38:55 -0500

    Exposed API key on the frontend under accounts
    Fixes #114

--- extensions/allauth_ex/urls.py
+++ extensions/allauth_ex/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', include('allauth.urls')),
-  path('delete/', views.account_delete, name='account_delete'),
+  path('delete/', views.DeleteAccountView.as_view(), name='account_delete'),
+  path('api_access/', views.APIAccessAccountView.as_view(), name='account_api_access'),
   path('github/', views.GithubLogin.as_view(), name='github_auth'),
   path('orcid/', views.OrcidLogin.as_view(), name='orcid_auth'),
   path('globus/', views.GlobusLogin.as_view(), name='globus_auth'),
--- extensions/allauth_ex/views.py
+++ extensions/allauth_ex/views.py
@@ -1,8 +1,10 @@
 from allauth.account.views import LogoutView
-from rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
-from allauth.socialaccount.providers.orcid.views import OrcidOAuth2Adapter
 from allauth.socialaccount.providers.globus.views import GlobusAdapter
+from allauth.socialaccount.providers.orcid.views import OrcidOAuth2Adapter
+from django.views.generic.base import TemplateResponseMixin, View
+from rest_auth.registration.views import SocialLoginView
+from rest_framework.authtoken.models import Token
 
 class GithubLogin(SocialLoginView):
   adapter_class = GitHubOAuth2Adapter
@@ -13,6 +15,20 @@ class OrcidLogin(SocialLoginView):
 class GlobusLogin(SocialLoginView):
   adapter_class = GlobusAdapter
 
+class APIAccessAccountView(TemplateResponseMixin, View):
+  template_name = "account/api_access.html"
+
+  def get(self, *args, **kwargs):
+    ctx = self.get_context_data()
+    return self.render_to_response(ctx)
+
+  def get_context_data(self, **kwargs):
+    context = kwargs
+    context['api_key'] = Token.objects.get_or_create(
+      user=self.request.user
+    )[0]
+    return context
+
 class DeleteAccountView(LogoutView):
   template_name = "account/delete.html"
   redirect_field_name = "next"
@@ -22,5 +38,3 @@ class DeleteAccountView(LogoutView):
     if user.is_authenticated:
       user.delete()
     return super().logout()
-
-account_delete = DeleteAccountView.as_view()
