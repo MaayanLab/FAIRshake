6a72d9e481cb1244dd3495d697f6476a5a5cb2ce
Daniel J. B. Clarke <u8sand@gmail.com>
2019-04-12 16:44:14 -0400

    Converted exceptions log to debug instead of warn

--- extensions/rest_framework_ex/exeptions.py
+++ extensions/rest_framework_ex/exeptions.py
@@ -10,7 +10,7 @@ def handler(exc, context):
     return response
   request = context['request']
 
-  logging.warn('exception occured', request, response, exc)
+  logging.debug('exception occured', request, response, exc)
   if all([
     'text/html' in request.META['HTTP_ACCEPT'],
     request.user.is_anonymous,
