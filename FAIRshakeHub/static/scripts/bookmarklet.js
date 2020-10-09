59753db177e5c593f273334f5e54532ada2a5766
Daniel Clarke <u8sand@gmail.com>
2020-10-09 15:59:13 -0400

    Adjust bookmarklet to link to search instead of assess directly

--- FAIRshakeHub/static/scripts/bookmarklet.js
+++ FAIRshakeHub/static/scripts/bookmarklet.js
@@ -19,7 +19,7 @@ require.config({
                     "<div id='fairness-bklet' style='font-size:12px;text-align:left;font-family:arial, helvetica, clean, sans-serif;line-height:15px;width:120px;background-color:white;position:fixed;right:0;top:0;z-index:9999;padding:10px;-moz-box-shadow:0px 0px 5px #333333;-webkit-box-shadow:0px 0px 5px #333333;box-shadow:0px 0px 5px #333333'>" +
                         "<div id='fairness-bklet-insig' style='width:40px; height:40px; margin:0 auto; padding-bottom:10px; shape-rendering:crispEdges;z-index:10000'></div>" +
                         "<div id='fairshakeBkletInfo' style='text-align:center;word-wrap:break-word;'>" +
-                            "<a id='fairshakeBkletLink' href='https://fairshake.cloud/assessment/add/?target__url="+encodeURIComponent(browserURL)+"&target__title="+encodeURIComponent(document.title)+"' style='font-family:arial;line-height:15px;font-size:12px;color:#337ab7;'>Submit Evaluation</a>" +
+                            "<a id='fairshakeBkletLink' href='https://fairshake.cloud/?q="+encodeURIComponent(browserURL)+"&projects=1&digitalobjects=1&rubrics=1&metrics=1' style='font-family:arial;line-height:15px;font-size:12px;color:#337ab7;'>View on FAIRshake</a>" +
                         "</div>" +
                         "<a id='close-overlay' onclick='removeBklet();' href='javascript:void(0)' style='line-height:10px;text-decoration:none;position:absolute;top:0;right:0;background:#eee;font-size:0.9em;padding:2px 5px;border:solid #ccc;border-width:0 0 1px 1px;display:block;'>X</a>" +
                     "</div>" +
