c8ea7e3faaa4fbd0d578e18d69c356dca322f0bf
Daniel J. B. Clarke <u8sand@gmail.com>
2021-06-22 20:53:29 -0400

    Update url_similar to reflect search vector

--- FAIRshakeAPI/lookups.py
+++ FAIRshakeAPI/lookups.py
@@ -1,6 +1,9 @@
 from django.db.models import Lookup
 from django.db.models.fields import TextField
 
+def _sql_length(s):
+  return 'LENGTH({s})'.format(s=s)
+
 def _sql_concat(lhs, rhs):
   return '{lhs} || {rhs}'.format(lhs=lhs, rhs=rhs)
 
@@ -38,39 +41,40 @@ def _sql_case(*kargs):
 class UrlSimilar(Lookup):
   lookup_name = 'url_similar'
 
-  def as_sql(self, compiler, connection, _concat=_sql_concat, _is_instr=_sql_is_instr, **kwargs):
+  def as_sql(self, compiler, connection, **kwargs):
     lhs, lhs_params = self.process_lhs(compiler, connection)
     rhs, rhs_params = self.process_rhs(compiler, connection)
-    params = (lhs_params*1) + (rhs_params*3)
-    return _is_instr(
+    params = (lhs_params*3) + (rhs_params*1)
+    lhs_norm = _sql_replace(
       _sql_replace(
-        _sql_replace(
-          lhs,
-          "'http://'",
-          "'https://'"
-        ),
-        "'https://www.'",
+        lhs,
+        "'http://'",
         "'https://'"
       ),
+      "'https://www.'",
+      "'https://'"
+    )
+    rhs_norm = _sql_replace(
       _sql_replace(
-        _sql_replace(
-          _sql_case(
-            dict(when=_is_instr(rhs, "'://'"), then=rhs),
-            _concat("'https://identifiers.org/'", rhs)
-          ),
-          "'http://'",
-          "'https://'"
-        ),
-        "'https://www.'",
+        rhs,
+        "'http://'",
         "'https://'"
+      ),
+      "'https://www.'",
+      "'https://'"
+    )
+    return '{} - {} > 0'.format(
+      _sql_length(lhs_norm),
+      _sql_length(
+        _sql_replace(lhs_norm, rhs_norm, "''")
       ),
     ), params
 
-  def as_mysql(self, *args, _concat=_mysql_concat, _is_instr=_mysql_is_instr, **kwargs):
-    return self.as_sql(*args, _concat=_concat, _is_instr=_is_instr, **kwargs)
+  def as_mysql(self, *args, **kwargs):
+    return self.as_sql(*args, **kwargs)
 
-  def as_postgresql(self, *args, _concat=_psql_concat, _is_instr=_psql_is_instr, **kwargs):
-    return self.as_sql(*args, _concat=_concat, _is_instr=_is_instr, **kwargs)
+  def as_postgresql(self, *args, **kwargs):
+    return self.as_sql(*args, **kwargs)
 
 @TextField.register_lookup
 class UrlStrict(Lookup):
